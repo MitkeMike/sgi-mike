87d63ea2651cb0e3b260283d4a8c48e33bcb80a8
Mike Mitchel <memb8631@gmail.com>
2024-05-16 18:12:37 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'SGI_Mike',
+  webDir: 'www'
+};
+
+export default config;
